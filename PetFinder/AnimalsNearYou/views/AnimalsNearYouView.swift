1ef63dc2c864b93ce2e77c5f4d3574167092e333
Djihane Ghilani <djihaneghilani@gmail.com>
2025-09-10 17:57:19 +0100

    Update : updated Animals Near You View to consume data from View Model

--- PetFinder/AnimalsNearYou/views/AnimalsNearYouView.swift
+++ PetFinder/AnimalsNearYou/views/AnimalsNearYouView.swift
@@ -9,69 +9,54 @@ import SwiftUI
 
 struct AnimalsNearYouView: View {
     @SectionedFetchRequest<String, AnimalEntity>(
-      sectionIdentifier: \AnimalEntity.animalSpecies,
-      sortDescriptors: [
-        NSSortDescriptor(keyPath: \AnimalEntity.species, ascending: true),
-        NSSortDescriptor(keyPath: \AnimalEntity.timestamp, ascending: true)
-      ],
-      animation: .default
+        sectionIdentifier: \AnimalEntity.animalSpecies,
+        sortDescriptors: [
+            NSSortDescriptor(keyPath: \AnimalEntity.species, ascending: true),
+            NSSortDescriptor(keyPath: \AnimalEntity.timestamp, ascending: true)
+        ],
+        animation: .default
     ) private var sectionedAnimals: SectionedFetchResults<String, AnimalEntity>
     
-  @State var isLoading = true
-  private let requestManager = RequestManager()
-
-  var body: some View {
-    NavigationView {
-      List {
-        ForEach(sectionedAnimals) { animals in
-            Section(header: Text(animals.id))  { 
-                ForEach (animals) { animal in
-                    NavigationLink (destination: AnimalDetailsView()) {
-                        AnimalRow(animal: animal)
+    @ObservedObject var viewModel: AnimalsNearYouViewModel
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(sectionedAnimals) { animals in
+                    Section(header: Text(animals.id))  {
+                        ForEach (animals) { animal in
+                            NavigationLink (destination: AnimalDetailsView()) {
+                                AnimalRow(animal: animal)
+                            }
+                        }
                     }
+                    
                 }
             }
-                    
-        }
-      }
-      .task {
-        await fetchAnimals()
-      }
-      .listStyle(.plain)
-      .navigationTitle("Animals near you")
-      .overlay {
-        if isLoading {
-          ProgressView("Finding Animals near you...")
-        }
-      }
-    }.navigationViewStyle(StackNavigationViewStyle())
-  }
-
-  func fetchAnimals() async {
-    do {
-      let animalsContainer: AnimalsContainer = try await requestManager.perform(AnimalsRequest.getAnimalsWith(
-        page: 1,
-        latitude: nil,
-        longitude: nil))
-      //let animals = animalsContainer.animals
-      //self.animals = animals
-        for var animal in animalsContainer.animals {
-          animal.toManagedObject()
-        }
-      await stopLoading()
-    } catch {
+            .task {
+                await viewModel.fetchAnimals()
+            }
+            .listStyle(.plain)
+            .navigationTitle("Animals near you")
+            .overlay {
+                if viewModel.isLoading {
+                    ProgressView("Finding Animals near you...")
+                }
+            }
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
-  }
-
-  @MainActor
-  func stopLoading() async {
-    isLoading = false
-  }
+    
+    
+    
+    @MainActor
+    func stopLoading() async {
+        viewModel.isLoading = false
+    }
 }
-
+ 
 struct AnimalsNearYouView_Previews: PreviewProvider {
-  static var previews: some View {
-      AnimalsNearYouView(isLoading: false)
-        .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-  }
+    static var previews: some View {
+        AnimalsNearYouView(viewModel: AnimalsNearYouViewModel()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
 }
